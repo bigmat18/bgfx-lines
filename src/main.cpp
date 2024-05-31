b3593ccc21d4570404dbdb48540b3974d3862ff4
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2024-05-31 21:51:24 +0200

    Sync

--- src/main.cpp
+++ src/main.cpp
@@ -57,9 +57,16 @@ namespace {
             bool update() override {
 
                 if (!entry::processEvents(m_width, m_height, m_debug, m_reset, &m_mouseState)) {
-                    imguiBeginFrame(m_mouseState.m_mx, m_mouseState.m_my, (m_mouseState.m_buttons[entry::MouseButton::Left] ? IMGUI_MBUT_LEFT : 0) | (m_mouseState.m_buttons[entry::MouseButton::Right] ? IMGUI_MBUT_RIGHT : 0) | (m_mouseState.m_buttons[entry::MouseButton::Middle] ? IMGUI_MBUT_MIDDLE : 0), m_mouseState.m_mz, uint16_t(m_width), uint16_t(m_height));
+                    imguiBeginFrame(m_mouseState.m_mx, 
+                                    m_mouseState.m_my, 
+                                    (m_mouseState.m_buttons[entry::MouseButton::Left] ? IMGUI_MBUT_LEFT : 0) | 
+                                    (m_mouseState.m_buttons[entry::MouseButton::Right] ? IMGUI_MBUT_RIGHT : 0) | 
+                                    (m_mouseState.m_buttons[entry::MouseButton::Middle] ? IMGUI_MBUT_MIDDLE : 0), 
+                                    m_mouseState.m_mz, 
+                                    uint16_t(m_width), 
+                                    uint16_t(m_height));
 
-                    showExampleDialog(this);
+                    // showExampleDialog(this);
 
                     imguiEndFrame();
 
@@ -75,14 +82,16 @@ namespace {
 
                     const bgfx::Stats *stats = bgfx::getStats();
 
-                    bgfx::dbgTextImage(
-                        bx::max<uint16_t>(uint16_t(stats->textWidth / 2), 20) - 20, bx::max<uint16_t>(uint16_t(stats->textHeight / 2), 6) - 6, 40, 12, s_logo, 160);
-                    bgfx::dbgTextPrintf(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
+                    bgfx::dbgTextImage(bx::max<uint16_t>(uint16_t(stats->textWidth / 2), 20) - 20, 
+                                       bx::max<uint16_t>(uint16_t(stats->textHeight / 2), 6) - 6, 
+                                       40, 12, s_logo, 160);
+                                       
+                    // bgfx::dbgTextPrintf(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
 
-                    bgfx::dbgTextPrintf(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
-                    bgfx::dbgTextPrintf(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
+                    // bgfx::dbgTextPrintf(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
+                    // bgfx::dbgTextPrintf(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
 
-                    bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.", stats->width, stats->height, stats->textWidth, stats->textHeight);
+                    // bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.", stats->width, stats->height, stats->textWidth, stats->textHeight);
 
                     // Advance to next frame. Rendering thread will be kicked to
                     // process submitted rendering primitives.
