c03e11fe8b37079f9105084b914c08a1947eb73c
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2024-05-31 23:20:26 +0200

    Try some rendering

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,13 @@
 #include <bgfx_utils.h>
 #include <imgui/imgui.h>
 #include "logo.h"
+#include <iostream>
 
 namespace {
 
-    class ExampleHelloWorld : public entry::AppI {
+    class TestWindow : public entry::AppI {
         public:
-            ExampleHelloWorld(const char *_name, const char *_description, const char *_url)
+            TestWindow(const char *_name, const char *_description, const char *_url)
                 : entry::AppI(_name, _description, _url)
             {}
 
@@ -26,7 +27,7 @@ namespace {
                 m_reset = BGFX_RESET_VSYNC;
 
                 bgfx::Init init;
-                init.type = args.m_type;
+                init.type = bgfx::RendererType::OpenGL;
                 init.vendorId = args.m_pciId;
                 init.platformData.nwh = entry::getNativeWindowHandle(entry::kDefaultWindowHandle);
                 init.platformData.ndt = entry::getNativeDisplayHandle();
@@ -36,21 +37,16 @@ namespace {
                 init.resolution.reset = m_reset;
                 bgfx::init(init);
 
-                // Enable debug text.
                 bgfx::setDebug(m_debug);
-
-                // Set view 0 clear state.
                 bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x303030ff, 1.0f, 0);
 
                 imguiCreate();
+                std::cout << bgfx::getRendererType() << std::endl;
             }
 
             virtual int shutdown() override {
                 imguiDestroy();
-
-                // Shutdown bgfx.
                 bgfx::shutdown();
-
                 return 0;
             }
 
@@ -65,36 +61,18 @@ namespace {
                                     m_mouseState.m_mz, 
                                     uint16_t(m_width), 
                                     uint16_t(m_height));
-
-                    // showExampleDialog(this);
-
                     imguiEndFrame();
 
-                    // Set view 0 default viewport.
                     bgfx::setViewRect(0, 0, 0, uint16_t(m_width), uint16_t(m_height));
-
-                    // This dummy draw call is here to make sure that view 0 is cleared
-                    // if no other draw calls are submitted to view 0.
                     bgfx::touch(0);
-
-                    // Use debug font to print information about this example.
                     bgfx::dbgTextClear();
 
                     const bgfx::Stats *stats = bgfx::getStats();
-
                     bgfx::dbgTextImage(bx::max<uint16_t>(uint16_t(stats->textWidth / 2), 20) - 20, 
                                        bx::max<uint16_t>(uint16_t(stats->textHeight / 2), 6) - 6, 
                                        40, 12, s_logo, 160);
-                                       
-                    // bgfx::dbgTextPrintf(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
 
-                    // bgfx::dbgTextPrintf(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
-                    // bgfx::dbgTextPrintf(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
 
-                    // bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.", stats->width, stats->height, stats->textWidth, stats->textHeight);
-
-                    // Advance to next frame. Rendering thread will be kicked to
-                    // process submitted rendering primitives.
                     bgfx::frame();
 
                     return true;
@@ -113,7 +91,7 @@ namespace {
 }
 
 ENTRY_IMPLEMENT_MAIN(
-    ExampleHelloWorld, 
-    "", 
-    "", 
-    "");
+    TestWindow, 
+    "TestWindow", 
+    "BGFX lines", 
+    "BGFX lines");
