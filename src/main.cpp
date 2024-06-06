1cf2b55514296e1190daad6d159361e382ec89c9
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2024-06-06 13:24:40 +0200

    Some test

--- src/main.cpp
+++ src/main.cpp
@@ -30,30 +30,32 @@ namespace {
     
     bgfx::VertexLayout PosColorVertex::ms_layout;
 
-    static PosColorVertex s_cubeVertices[] = {
-        {-1.0f,  1.0f,  1.0f, 0xff000000 },
-        { 1.0f,  1.0f,  1.0f, 0xff0000ff },
-        {-1.0f, -1.0f,  1.0f, 0xff00ff00 },
-        { 1.0f, -1.0f,  1.0f, 0xff00ffff },
-        {-1.0f,  1.0f, -1.0f, 0xffff0000 },
-        { 1.0f,  1.0f, -1.0f, 0xffff00ff },
-        {-1.0f, -1.0f, -1.0f, 0xffffff00 },
-        { 1.0f, -1.0f, -1.0f, 0xffffffff },
+    static PosColorVertex s_squareVertices[] =
+    {
+        {-1.0f,  1.0f,  1.0f, 0xff000000 }, // 0
+        { 1.0f,  1.0f,  1.0f, 0xff0000ff }, // 1
+        {-1.0f, -1.0f,  1.0f, 0xff00ff00 }, // 2
+        { 1.0f, -1.0f,  1.0f, 0xff00ffff }, // 3
+        {-1.0f,  1.0f, -1.0f, 0xffff0000 }, // 4
+        { 1.0f,  1.0f, -1.0f, 0xffff00ff }, // 5
+        {-1.0f, -1.0f, -1.0f, 0xffffff00 }, // 6
+        { 1.0f, -1.0f, -1.0f, 0xffffffff }, // 7
     };
     
 
-    static const uint16_t s_cubeTriList[] = {
-        0, 1, 2, // 0
+    static const uint16_t s_squareIndeces[] =
+    {
+        0, 1, 2,
         1, 3, 2,
-        4, 6, 5, // 2
+        4, 6, 5,
         5, 6, 7,
-        0, 2, 4, // 4
+        0, 2, 4,
         4, 2, 6,
-        1, 5, 3, // 6
+        1, 5, 3,
         5, 7, 3,
-        0, 4, 1, // 8
+        0, 4, 1,
         4, 5, 1,
-        2, 3, 6, // 10
+        2, 3, 6,
         6, 3, 7,
     };
 
@@ -87,32 +89,13 @@ namespace {
 
                 PosColorVertex::init();
 
-                std::vector<PosColorVertex> vertices;
-                std::vector<uint16_t> indices;
-
-                for(int y = 0; y < 100; ++y) {
-                    for(int x = 0; x < 100; ++x) {
-                        vertices.push_back({-1.0f + floorf(2.0f / 100.0f) * x, 1.0f * floorf(2.0f / 100.0f) * x, 0.0f, 0xff000000});
-                        
-                        if(y == 0 || x == 0) {
-                            indices.push_back(x);
-                            indices.push_back(x+1);
-                            indices.push_back(y+1);
-                        }
-
-                        if(y != 100 && x != 0) {
-
-                        }
-                    }
-                }
-
                 m_vbh = bgfx::createVertexBuffer(
-                    bgfx::makeRef(&vertices[0], vertices.size() * sizeof(vertices[0])),
+                    bgfx::makeRef(s_squareVertices, sizeof(s_squareVertices)),
                     PosColorVertex::ms_layout
                 );
 
                 m_ibh = bgfx::createIndexBuffer(
-                    bgfx::makeRef(s_cubeTriList, sizeof(s_cubeTriList))
+                    bgfx::makeRef(s_squareIndeces, sizeof(s_squareIndeces))
                 );
 
                 m_program = loadProgram("vs_base", "fs_base");
@@ -155,7 +138,7 @@ namespace {
                     float time = (float)((bx::getHPCounter() - m_timeOffset) / double(bx::getHPFrequency()));
 
                     const bx::Vec3 at = {0.0f, 0.0f, 0.0f};
-                    const bx::Vec3 eye = {0.0f, 0.0f, -35.0f};
+                    const bx::Vec3 eye = {0.0f, 0.0f, -20.0f};
 
                     {
                         float view[16];
@@ -168,20 +151,16 @@ namespace {
                         bgfx::setViewRect(0, 0, 0, uint16_t(m_width), uint16_t(m_height));
                     }
 
-                    bgfx::touch(0);
+                    // bgfx::touch(0);
 
-                    uint64_t state = 0
-                        | BGFX_STATE_WRITE_R
-                        | BGFX_STATE_WRITE_G
-                        | BGFX_STATE_WRITE_B
-                        | BGFX_STATE_WRITE_A
-                        | BGFX_STATE_WRITE_Z
-                        | BGFX_STATE_DEPTH_TEST_LESS
-                        | BGFX_STATE_CULL_CW
-                        | BGFX_STATE_MSAA
-                        | UINT64_C(0);
+                    float mtx[16];
+                    bx::mtxRotateX(mtx, time);
+                    bgfx::setTransform(mtx);
 
+                    bgfx::setVertexBuffer(0, m_vbh);
+                    bgfx::setIndexBuffer(m_ibh);
 
+                    bgfx::submit(0, m_program);
 
                     bgfx::frame();
 
