026fb20d6ade2d5d86abdf6abfdd31bb4657f39c
bigmat18 <mat.giu2002@gmail.com>
2025-03-19 20:15:37 +0100

    Adding camera for examples

--- examples/02-polylines/main.cpp
+++ examples/02-polylines/main.cpp
@@ -1,6 +1,7 @@
 #define ENTRY_CONFIG_IMPLEMENT_MAIN 1
 #include <bgfx_utils.h>
 #include <common.h>
+#include <camera.h>
 #include <polylines.hpp>
 
 #include "../common/generator.h"
@@ -35,6 +36,9 @@ namespace
             bgfx::setDebug(m_debug);
             bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0xFFFFFFFF, 1.0f, 0);
 
+            cameraCreate();
+            cameraSetPosition({0.0f, 0.0f, -5.0f});
+
             std::vector<lines::LinesVertex> points;
             generatePointsInCube(points, 3, 100);
 
@@ -51,6 +55,16 @@ namespace
         {
             if (!entry::processEvents(m_width, m_height, m_debug, m_reset, &m_mouseState))
             {
+                cameraCoordinatesUpdate();
+                float proj[16];
+                // bx::mtxProj(proj, 60.0f, float(m_width) / float(m_height), 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
+                bx::mtxOrtho(proj, left, right, bottom, top, nearPlane, farPlane, 0.0f, false);
+                
+                float view[16];
+                cameraGetViewMtx(view);
+
+                bgfx::setViewTransform(0, view, proj);
+
                 bgfx::setViewRect(0, 0, 0, uint16_t(m_width), uint16_t(m_height));
                 bgfx::touch(0);
 
@@ -64,12 +78,34 @@ namespace
             return false;
         }
 
+        void cameraCoordinatesUpdate() {
+            int64_t now = bx::getHPCounter();
+            static int64_t last = now;
+            const int64_t frameTime = now - last;
+            last = now;
+
+            const double freq = double(bx::getHPFrequency());
+            const float time = (float)((now - m_timeOffset) / double(bx::getHPFrequency()));
+            const float deltaTime = float(frameTime / freq);
+
+            cameraUpdate(deltaTime * 0.1, m_mouseState);
+        }
+
         entry::MouseState m_mouseState;
 
         uint32_t m_width;
         uint32_t m_height;
         uint32_t m_debug;
         uint32_t m_reset;
+
+        uint64_t m_timeOffset;
+
+        float left = -10.0f;
+        float right = 10.0f;
+        float bottom = -10.0f;
+        float top = 10.0f;
+        float nearPlane = -10.0f;
+        float farPlane = 10.0f;
 
         std::unique_ptr<lines::Polylines> line;
     };
